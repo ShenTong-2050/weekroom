063b0b43dd5023d27de9113b3a1f33f754d4ed4a
ShenTongtong <sttya@qq.com>
2023-04-07 10:16:27 +0800

    增加http.NewFileServe测试文件

--- test/daily-package/web/http_fileserver.go
+++ test/daily-package/web/http_fileserver.go
@@ -1,5 +1,87 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+var (
+	FilePathName string
+)
+
+func init() {
+	flag.StringVar(&FilePathName,"fpn","./","the file name")
+}
+
 func main() {
-	$END$
+
+	flag.Parse()
+
+	mux := http.NewServeMux()
+
+	// 将地址前缀 /static 替换为 /public
+	mux.Handle("/static/",http.StripPrefix("/static",http.FileServer(http.Dir("./public"))))
+	//mux.Handle("/static/",http.FileServer(http.Dir("")))
+	// 通过 http.ServeContent 直接输出文件内容
+	mux.HandleFunc("/get",FileContentHandle)
+	// 通过 命令行输入 参数 下载文件
+	mux.Handle("/download/",http.StripPrefix("/download/",http.FileServer(http.Dir(FilePathName))))
+
+	// mux.HandleFunc("/test/",testPath)
+
+	server := http.Server{Addr: ":8080",Handler: mux}
+	if err := server.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
+
+// FileContentHandle 输出文件内容
+func FileContentHandle(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	fileName := query.Get("show")
+	if fileName == "" {
+		w.WriteHeader(400)
+		fmt.Fprintln(w,"filename is empty")
+		return
+	}
+	GetFileContent(w,r,fileName,time.Time{})
+}
+
+// GetFileContent 读取文件内容函数
+func GetFileContent(w http.ResponseWriter, r *http.Request,name string, time time.Time) {
+	// 打开文件获取句柄
+	f,err := os.Open(name)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintln(w,"open file error, ", err)
+		return
+	}
+	defer f.Close()
+
+	fs,err := f.Stat()
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintln(w,"get file message failed, ",err)
+		return
+	}
+
+	if fs.IsDir() {
+		w.WriteHeader(500)
+		fmt.Fprintln(w,"no such file ")
+		return
+	}
+
+	http.ServeContent(w,r,name,fs.ModTime(),f)
+}
+
+func testPath (w http.ResponseWriter, r *http.Request) {
+	// 通过 http.DetectContentType 获取返回的 Content-type 类型
+	path := "/static/dir1/img.png"
+	ext := filepath.Ext(path)
+	fmt.Fprintln(w,ext)
+	w.WriteHeader(http.StatusOK)
 }
--- test/daily-package/web/web.go
+++ test/daily-package/web/web.go
@@ -1,5 +1,113 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	netUrl "net/url"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+)
+
+var ExtToContentType map[string]string = map[string]string{
+	".html":"text/html;charset=utf-8",
+	".js":"application/javascript",
+	".css":"text/css;charset=utf-8",
+	".xml":"text/xml;charset=utf-8",
+	".jpg":"image/jpeg",
+}
+
 func main() {
-	$END$
+	mux := http.NewServeMux()
+	mux.HandleFunc("/static/",FileHandle)
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintln(writer,"hello world!")
+	})
+	server := &http.Server{Addr: ":8080",Handler: mux}
+	if err := server.ListenAndServe(); err != nil {
+		toHttpError(err)
+	}
+}
+
+// FileHandle 文件处理程序
+func FileHandle(w http.ResponseWriter,r *http.Request) {
+	// 通过 url 获取请求地址 . 的作用是指相对路径
+	path := "."+r.URL.Path
+
+	// 打开文件
+	f,err := os.Open(path)
+	if err != nil {
+		Error(w,toHttpError(err))
+		return
+	}
+	defer f.Close()
+
+	// 获取文件信息
+	stat,err := f.Stat()
+	if err != nil {
+		Error(w,toHttpError(err))
+		return
+	}
+
+	// 判断 是否为目录 是则遍历
+	if stat.IsDir() {
+		DirList(w,r,f)
+	}
+
+	// 读取所有文件内容
+	data,err := ioutil.ReadAll(f)
+	if err != nil {
+		Error(w,toHttpError(err))
+		return
+	}
+
+	// 根据文件后缀设置对应的文件响应类型 与 文件长度
+	ext := filepath.Ext(path)
+	// if contentType := ExtToContentType[ext]; contentType != "" {
+	if contentType := http.DetectContentType([]byte(ext)); contentType != "" {
+		w.Header().Set("Content-type",contentType)
+	}
+	w.Header().Set("Content-length",strconv.FormatInt(stat.Size(),10))
+	w.Write(data)
+}
+
+// DirList 遍历目录
+func DirList(w http.ResponseWriter,r *http.Request,f http.File) {
+	dirs,err := f.Readdir(-1)
+	if err != nil {
+		Error(w,toHttpError(err))
+		return
+	}
+
+	// 按照文件名称排序
+	sort.Slice(dirs,func(i,j int) bool { return dirs[i].Name() < dirs[j].Name() })
+
+	w.Header().Set("Content-type","charset=utf-8;charset=utf-8")
+
+	fmt.Fprintf(w,"<pre>\n")
+	for _,item := range dirs {
+		// 获取 文件/目录 名称
+		name := item.Name()
+		if item.IsDir() {
+			name+="/"
+		}
+		url := netUrl.URL{Path: name}
+		fmt.Fprintf(w,"<a href=\"%s\">%s</a>\n",url.String(),name)
+	}
+	fmt.Fprintf(w,"</pre>\n")
+}
+
+func toHttpError(err error) int {
+	if os.IsNotExist(err) {
+		return http.StatusNotFound
+	} else if os.IsPermission(err) {
+		return http.StatusForbidden
+	}
+	return http.StatusInternalServerError
+}
+
+func Error(w http.ResponseWriter, code int) {
+	w.WriteHeader(code)
 }

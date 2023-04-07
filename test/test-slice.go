1df63c59c41aba0a31ad9f7a9258af41eeb47769
ShenTongtong <sttya@qq.com>
2023-04-07 10:14:12 +0800

    测试切片容量扩容规律

---
+++ test/test-slice.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var a = make([]int,0,1024)
+
+	/*b := append(a,2)
+	c := append(a,1)*/
+
+	fmt.Println(a)
+	// 在没有扩容之前 b 与 c 指向的底层切片 都是 a
+	/*fmt.Println(b)
+	fmt.Println(c)*/
+	// 由于 d 操作给 a 中追加了三个元素 超过了 a 切片的容量, 所以拷贝了一份底层数组
+	// 在没有超过 1024 个元素时 capacity 是成倍增加、如果超过 1024 则是以 1024/4 的指数增加
+	e := []int{1,2}
+	d := append(a,e...)
+	fmt.Println(d)
+	for i:=0; i<1288; i++ {
+		d = append(d,i)
+	}
+	fmt.Println(cap(d))
+}
